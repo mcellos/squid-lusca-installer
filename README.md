4168eedfd181feca743bf9c6ef26ce0e29befdfd
Omega Hanggara <hanggara33@gmail.com>
2013-06-13 14:10:00 +0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# SQUID LUSCA INSTALLER
+
+### Simple Bash Script to install SQUID LUSCA to your LINUX OS
+## This repository is not finished yet, so it won't install SQUID LUSCA to your LINUX OS (yet)
+## You can subscribe this repository's progress by following my twitter at https://www.twitter.com/omegahanggara
+## I will give you some informations there as soon as there is (are) an update(s)
+
+```
+$ git clone git@github.com:omegahanggara/squid-lusca-installer.git
+$ cd squid-lusca-installer
+$ bash src/squid-lusca-installer.sh
+```
+
+If you found bug(s), please let me know by send me an email on hanggara33@gmail.com
+Any suggest(s) is (are) appreciated
+
+## This repository is under maintenance by [omegahanggara](https://github.com/omegahanggara)
+## Find me on twitter [omegahanggara](https://twitter.com/omegahanggara)
---
+++ src/squid-lusca-installer.sh
@@ -0,0 +1,119 @@
+#!/usr/bin/env bash
+
+#------------------------ Variable -----------------------------#
+
+#------------------------ Function -----------------------------#
+
+cin() {
+	if [ "$1" == "action" ] ; then output="\e[01;32m[>]\e[00m" ; fi
+	if [ "$1" == "info" ] ; then output="\e[01;33m[i]\e[00m" ; fi
+	if [ "$1" == "warning" ] ; then output="\e[01;31m[w]\e[00m" ; fi
+	if [ "$1" == "error" ] ; then output="\e[01;31m[e]\e[00m" ; fi
+	output="$output $2"
+	echo -en "$output"
+}
+ 
+cout() {
+	if [ "$1" == "action" ] ; then output="\e[01;32m[>]\e[00m" ; fi
+	if [ "$1" == "info" ] ; then output="\e[01;33m[i]\e[00m" ; fi
+	if [ "$1" == "warning" ] ; then output="\e[01;31m[w]\e[00m" ; fi
+	if [ "$1" == "error" ] ; then output="\e[01;31m[e]\e[00m" ; fi
+	output="$output $2"
+	echo -e "$output"
+}
+
+function checkRoot()
+{
+	if [[ $(whoami) != "root" ]]; then
+		cout error "You don't have root privilege!"
+		cout action "Quiting..."
+		sleep 2
+		exit 1
+	fi
+}
+
+function interrupt()
+{
+	echo -e "\n"
+	cout error "CAUGHT INTERRUPT SIGNAL!!!"
+	askToQuit=true
+	while [[ $askToQuit == "true" ]]; do
+		cin info "Do you really want to exit? (Y/n) "
+		read answer
+		if [[ $answer == *[Yy]* ]] || [[ $answer == "" ]]; then
+			cout action "WTF!!!"
+			sleep 1
+			exit 0
+		elif [[ $answer == *[Nn]* ]]; then
+			cout action "Rock on..."
+			askToQuit=false
+		fi
+	done
+}
+
+function setTerminal()
+{
+	cout action "Setup your default terminal..."
+	sleep 1
+	which terminator > /dev/null
+	if [[ $(echo $?) -eq 0 ]]; then
+		terminal=terminator
+		cout info "Setup terminator as your terminal..."
+	else
+		cout error "Terminator not found! Finding another one..."
+		sleep 1
+		which gnome-terminal > /dev/null
+		if [[ $(echo $?) -eq 0 ]]; then
+			terminal=gnome-terminal
+			cout info "Setup gnome-terminal as your terminal..."
+		else
+			cout error "gnome-terminal not found! Finding another one..."
+			sleep 1
+			which konsole > /dev/null
+			if [[ $(echo $?) -eq 0 ]]; then
+				terminal=konsole
+				cout info "Setup konsole as your terminal..."
+			else
+				cout error "konsole not found! Finding another one..."
+				which xterm > /dev/null
+				if [[ $(echo $?)  -eq 0 ]]; then
+					terminal=xterm
+					cout info "Setup xterm as your terminal..."
+				else
+					cout error "xterm not found!"
+					if [[ $terminal == "" ]]; then
+						cout error "Looks like you don't have any terminal installed on your system. Make sure you have one of them, them execute this script again."
+						cout action "Quiting..."
+						sleep 2
+						exit 1
+					fi
+				fi
+			fi
+		fi
+	fi
+}
+
+function openTerminal()
+{
+	terminalCMD=$($terminal -e "$cmd")
+}
+
+function testTerminal()
+{
+	cout action "Testing your terminal..."
+	sleep 1
+	cmd="whoami; sleep 3"
+	openTerminal > /dev/null 2>&1
+	if [[ $? -eq 0 ]]; then
+		cout info "Looks good..."
+		sleep 1
+	else
+		cout error "Looks not good... It's OK tho, but you may experience some problems on installation..."
+	fi
+}
+
+#------------------------ Main Program -----------------------------#
+trap 'interrupt' INT
+checkRoot
+setTerminal
+testTerminal
